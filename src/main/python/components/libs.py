e1ac7b6d75e4d23f3fd4fbc84fb7ed84e99d84e3
Leow Chee Siang <cheesiangleow5284@gmail.com>
2019-01-17 00:02:11 +0900

    changed vocoder function

--- src/main/python/components/libs.py
+++ src/main/python/components/libs.py
@@ -12,27 +12,32 @@ class MicrophoneRecorder:
                             channels=1,
                             rate=FS,
                             input=True,
-                            output=write_flag,
-                            frames_per_buffer=CHUNKS,)
+                            output=write_flag)
+                            # frames_per_buffer=CHUNKS)
         self.write_flag = write_flag
+
+
+
+    def audio_synthesize(self,y):
+        y = np.asarray(y,dtype=np.float)
+        before_shape = y.shape
+        # print("y_after",y.shape)
+        _f0, t = pw.dio(y, FS)    # raw pitch extractor
+        f0 = pw.stonemask(y, _f0, t, FS)  # pitch refinement
+        sp = pw.cheaptrick(y, f0, t, FS)  # extract smoothed spectrogram
+        ap = pw.d4c(y, f0, t, FS)         # extract aperiodicity
+        data = pw.synthesize(f0*2, sp, ap, FS)
+        y_new = data[0:before_shape[0]]
+        y_new = np.asarray(y_new,dtype=np.int16)
+        return y_new
+
     def read(self):
         data = self.stream.read(CHUNKS)
         y = np.frombuffer(data,"int16")
-        # print("y_after",)
-        before_shape = y.shape
-        get_data = y.astype(np.float)  # WORLDはfloat前提のコードになっているのでfloat型にしておく
-        _f0, t = pw.dio(get_data, FS)    # raw pitch extractor
-        f0 = pw.stonemask(get_data, _f0, t, FS)  # pitch refinement
-        sp = pw.cheaptrick(get_data, f0, t, FS)  # extract smoothed spectrogram
-        ap = pw.d4c(get_data, f0, t, FS)         # extract aperiodicity
-        y = pw.synthesize(f0*2, sp, ap, FS)
-        y = np.asarray(y,dtype=np.float)
-        # print("y_after",y.shape)
-        y_new = y[0:before_shape[0]]
-        y_new = np.asarray(y_new,dtype=np.int16)
-        [signal.emit(y_new) for signal in self.signals]
+        y_new = self.audio_synthesize(y)
         if self.write_flag:
-            self.stream.write(y_new)
+            self.stream.write(y_new,CHUNKS)
+        [signal.emit(y_new) for signal in self.signals]
 
     def close(self):
         self.stream.stop_stream()
--- src/main/python/components/settings.py
+++ src/main/python/components/settings.py
@@ -1,2 +1,2 @@
 FS = 44100 #Hz
-CHUNKS = 1024*8 #samples
+CHUNKS = 1024*2 #samples
